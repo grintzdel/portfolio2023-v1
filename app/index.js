ff18f741c07882c300964ef6cab1d1250d7f629f
grintzdel <mathis.oudin@etu.uca.fr>
2024-02-24 02:25:16 +0100

    plain javascript project architecture

--- app/index.js
+++ app/index.js
@@ -1,3 +1,21 @@
-import random from 'images/random.jpg'
+import Home from 'pages/Home'
+import About from 'pages/About'
+import Detail from 'pages/Detail'
+import Collections from 'pages/Collections'
 
-console.log(random)
+class App {
+  constructor () {
+    console.log('App is running')
+  }
+
+  createPages () {
+    this.pages = {
+      home: new Home(),
+      about: new About(),
+      detail: new Detail(),
+      collections: new Collections()
+    }
+  }
+}
+
+new App()
