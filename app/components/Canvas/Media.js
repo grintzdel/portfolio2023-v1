7667f6f1e91b7775b4c1a6ba740c51ad7e635dc3
grintzdel <mathis.oudin@etu.uca.fr>
2024-05-01 17:46:16 +0200

    draggable gallery home page.

--- app/components/Canvas/Media.js
+++ app/components/Canvas/Media.js
@@ -4,12 +4,13 @@ import fragment from 'shaders/plane-fragment.glsl'
 import vertex from 'shaders/plane-vertex.glsl'
 
 export default class {
-  constructor ({ element, geometry, gl, index, scene }) {
+  constructor ({ element, geometry, gl, index, scene, sizes }) {
     this.element = element
     this.geometry = geometry
     this.gl = gl
     this.index = index
     this.scene = scene
+    this.sizes = sizes
 
     this.createTexture()
     this.createProgram()
@@ -26,7 +27,7 @@ export default class {
   }
 
   createProgram () {
-    this.program = new Program(this.gl,{
+    this.program = new Program(this.gl, {
       fragment,
       vertex,
       uniforms: {
@@ -43,6 +44,47 @@ export default class {
 
     this.mesh.setParent(this.scene)
 
-    this.mesh.position.x = this.index * this.mesh.scale.x
+    this.mesh.scale.x = 2
+  }
+
+  createBounds ({ sizes }) {
+    this.sizes = sizes
+
+    this.bounds = this.element.getBoundingClientRect()
+
+    this.updateScale()
+    this.updateX()
+    this.updateY()
+  }
+
+  onResize (sizes) {
+    this.createBounds(sizes)
+  }
+
+  updateScale () {
+    this.height = this.bounds.height / window.innerHeight
+    this.width = this.bounds.width / window.innerWidth
+
+    this.mesh.scale.x = this.sizes.width * this.width
+    this.mesh.scale.y = this.sizes.height * this.height
+  }
+
+  updateX (x = 0) {
+    this.x = (this.bounds.left + x) / window.innerWidth
+
+    this.mesh.position.x = (-this.sizes.width / 2) + (this.mesh.scale.x / 2) + (this.x * this.sizes.width)
+  }
+
+  updateY (y = 0) {
+    this.y = (this.bounds.top + y) / window.innerHeight
+
+    this.mesh.position.y = (this.sizes.height / 2) - (this.mesh.scale.y / 2) - (this.y * this.sizes.height)
+  }
+
+  update (scroll) {
+    if (!this.bounds) return
+
+    this.updateX(scroll.x)
+    this.updateY(scroll.y)
   }
 }
